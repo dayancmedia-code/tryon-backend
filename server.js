3a47dd1172e6f25172c3dd097079dc2280a0a643
dayancmedia-code <dayancmedia@gmail.com>
2025-09-05 11:56:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Gerekli kütüphaneleri dahil etme
 const express = require('express');
+const app = express();
 app.use(express.json());
 const { createClient } = require('@supabase/supabase-js');
 const cors = require('cors');
